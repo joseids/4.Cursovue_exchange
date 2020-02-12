b390ca3f9246364366f18b702ce8003cc69b014b
joseTejeda <jtejedaids.git@gmail.com>
2020-02-12 13:14:13 -0600

    Cambios que si funcionan y que se ve muy bien

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
-  <main></main>
+  <main>
+    <px-header />
+    <px-assets-table />
+  </main>
 </template>
 
 <script>
+import PxHeader from "@/components/PxHeader";
+import PxAssetsTable from "@/components/PxAssetsTable";
+
 export default {
   name: "app",
-  components: {}
+  components: { PxHeader, PxAssetsTable }
 };
 </script>
 
